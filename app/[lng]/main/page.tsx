83e59876c252d665cf1ece2aeffc25dd8a8df5c7
Melany Arias <208660797>
2026-03-26 10:50:34 -0600

    Fix lng and main

--- app/[lng]/main/page.tsx
+++ app/[lng]/main/page.tsx
@@ -1,5 +1,13 @@
 import MainContent from "@/components/Main/MainContent";
+import { getDictionary } from "@/lib/get-dictionary";
 
-export default function Page() {
-  return <MainContent />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ lng: string }>;
+}) {
+  const { lng } = await params;
+  const dict = await getDictionary(lng as "en" | "es");
+
+  return <MainContent dict={dict} lng={lng} />;
 }
