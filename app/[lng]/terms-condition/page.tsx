c9dd7bcbd95ecfe9489dd738d07dbaf3716532b2
Melany Arias <208660797>
2026-04-09 12:42:00 -0600

    avance 9/4

--- app/[lng]/terms-condition/page.tsx
+++ app/[lng]/terms-condition/page.tsx
@@ -1,4 +1,4 @@
-import TermsContent from "@/components/terms&Condition/page";
+import TermsContent from "@/components/terms-condition/page";
 
 export default function Page() {
   return <TermsContent />;
